a34b793b142d9e93131cd43c3d280b6956573b68
Matteo Savatteri <matteo.savatteri@studenti.unimi.it>
2018-04-30 20:50:23 +0200

    Aggiunta funzione check_positive_int.
    Questa funzione controlla se il valore in entrata è un intero positivo.
    In caso contrario solleva un errore del parser. Scritta per migliorare
    il parsing degli argomenti.

--- testing/cn_math.py
+++ testing/cn_math.py
@@ -1,11 +1,20 @@
 # Questo modulo contiene le funzioni matematiche utilizzate nel progetto
 
 from math import sqrt
+from argparse import ArgumentTypeError
+
+def check_positive_int(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+         raise ArgumentTypeError("%s is not a positive int value" % value)
+    return ivalue
+
 
 def list_average(list):
 	"Returns the average of the elements of a float list"
 
 	return  sum(list)/len(list)
+
 
 def list_std_dev(list,list_average):
 	"Returns the standard deviation of the elements of a list, from their average"
