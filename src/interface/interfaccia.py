162f371154121b81fcf4dffc1572cd0b41c3ba62
Simone Daniotti <simonedaniotti@MacBook-Pro-di-Simone-2.local>
2018-05-09 20:33:55 +0200

    ho reso modulare la parte di definizione delle flags

--- src/interface/interfaccia.py
+++ src/interface/interfaccia.py
@@ -4,55 +4,19 @@
 #from __future__ import print_function
 import sys #modulo che gestisce l'interazione fra variabili passate e l'interprete
 import argparse
+import flag
 
 from lammps import lammps
-
 
 #Parser per le flags
 
 #The ArgumentParser object will hold all the information necessary to parse the command line into Python data types.
 parser = argparse.ArgumentParser(description='Python script interface able to generate\
-                                 lj+colored noise simulations',prefix_chars= '-')
-#Creo le flags
-parser.add_argument('--step', action='store', default=100, type=int, \
-                    help='passa il numero di simulazioni, un int che ha valore 100 di default') #optional argument
+                            lj+colored noise simulations',prefix_chars= '-')
 
-parser.add_argument('--mass', action='store', default=1.0, type=float, \
-                    help='passa il valore della massa delle particelle della simulazione, un float che ha valore 1.0 di default')
-
-parser.add_argument('--xx', action='store', default=20, type=int, \
-                    help='passa la lunghezza lungo x della nostra regione, un int che ha valore 20 di default')
-
-parser.add_argument('--yy', action='store', default=20, type=int, \
-                    help='passa la lunghezza lungo y della nostra regione, un int che ha valore 20 di default')
-
-parser.add_argument('--zz', action='store', default=20, type=int,\
-                    help='passa la lunghezza lungo z della nostra regione, un int che ha valore 20 di default')
-
-parser.add_argument('--thermo', action='store', default=10, type=int, \
-                    help='passa ogni quante simulazioni printare la termodinamica del mio sistema, un int che ha valore 10 di default')
-
-
-#['rock', 'paper', 'scissors']
-parser.add_argument('pot', choices=['yukawa'],  \
-                    help='passa il tipo di potenziale da accoppiare a lj')
-
-parser.add_argument('pot_coeff', action= 'append',type=float,nargs='+', help='passa i coefficienti richiesti dal potenziale: *)per yukawa sono screening_length, global_cutoff, A (energy*distance units), cutoff(locale, di quel preciso accoppiamento)')
-
-
-parser.add_argument('--if_dump_atom', action='store_true')
-
-parser.add_argument('--dump_atom', action='store', default=10, type=int, \
-                    help='passa ogni quante simulazioni salvare uno snapshot del sistema di atomi, un int che ha valore 10 di default')
-
-parser.add_argument('--if_dump_speed', action='store_true')
-
-parser.add_argument('--dump_speed', action='store', default=99, type=int, \
-                    help='passa ogni quante simulazioni salvare uno snapshot delle velocità degli atomi, un int che ha valore 100 di default')
-
+noise_flags.configure(parser)
 
 args = parser.parse_args()
-#NB:: per il potenziale: usare choices
 
 print("Arguments parsed correctly.")
 
---
+++ src/interface/noise_flags.py
@@ -0,0 +1,45 @@
+import argparse
+
+def configure(parser):
+
+
+#Creo le flags
+    parser.add_argument('--step', action='store', default=100, type=int, \
+                    help='passa il numero di simulazioni, un int che ha valore 100 di default') #optional argument
+
+    parser.add_argument('--mass', action='store', default=1.0, type=float, \
+                    help='passa il valore della massa delle particelle della simulazione, un float che ha valore 1.0 di default')
+
+    parser.add_argument('--xx', action='store', default=20, type=int, \
+                    help='passa la lunghezza lungo x della nostra regione, un int che ha valore 20 di default')
+
+    parser.add_argument('--yy', action='store', default=20, type=int, \
+                    help='passa la lunghezza lungo y della nostra regione, un int che ha valore 20 di default')
+
+    parser.add_argument('--zz', action='store', default=20, type=int,\
+                    help='passa la lunghezza lungo z della nostra regione, un int che ha valore 20 di default')
+
+    parser.add_argument('--thermo', action='store', default=10, type=int, \
+                    help='passa ogni quante simulazioni printare la termodinamica del mio sistema, un int che ha valore 10 di default')
+
+
+#['rock', 'paper', 'scissors']
+    parser.add_argument('pot', choices=['yukawa'],  \
+                    help='passa il tipo di potenziale da accoppiare a lj')
+
+    parser.add_argument('pot_coeff', action= 'append',type=float,nargs='+', help='passa i coefficienti richiesti dal potenziale: *)per yukawa sono screening_length, global_cutoff, A (energy*distance units), cutoff(locale, di quel preciso accoppiamento)')
+
+
+    parser.add_argument('--if_dump_atom', action='store_true')
+
+    parser.add_argument('--dump_atom', action='store', default=10, type=int, \
+                    help='passa ogni quante simulazioni salvare uno snapshot del sistema di atomi, un int che ha valore 10 di default')
+
+    parser.add_argument('--if_dump_speed', action='store_true')
+
+    parser.add_argument('--dump_speed', action='store', default=99, type=int, \
+                    help='passa ogni quante simulazioni salvare uno snapshot delle velocità degli atomi, un int che ha valore 100 di default')
+
+
+
+#NB:: per il potenziale: usare choices
