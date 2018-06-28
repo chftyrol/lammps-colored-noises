e834464cd9ce7a52c87736499a9975b718d62447
McDaniots <daniottisimone@gmail.com>
2018-06-28 16:24:07 +0200

    aggiunto lo script per il calcolo del raggio di girazione

---
+++ src/interface/Giration.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import math , sys
+import numpy as np
+
+#Calcola il raggio di girazione delle particelle, dato un file di input dalla fattezza di quelli dump.ecc
+def compute(file):
+    
+    coord = np.genfromtxt(file, delimiter=" ", skip_header=9, dtype=float)
+    Len= len(coord)
+    rm = [sum(i) for i in zip(*coord)]
+    rm=[rm[0]/Len,rm[1]/Len,rm[2]/Len]
+    
+    R=0
+    for i in range(0,Len):
+        R=R+(coord[i][0]-rm[0])**2+(coord[i][1]-rm[1])**2+(coord[i][2]-rm[2])**2
+    return(R/Len)
+
