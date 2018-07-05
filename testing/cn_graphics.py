409d7f936fd0b18518336d1127156fd01c9d4d63
Matteo Savatteri <matteo.savatteri@studenti.unimi.it>
2018-07-05 21:36:53 +0200

    Aggiunto modulo per la grafica.
    Aggiunto il modulo cn_graphics.py. Contiene le funzioni che permettono
    la generazione dei grafici dei dati in cntester.
    Le funzioni dipendono da "matplotlib".

---
+++ testing/cn_graphics.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+
+def plot_list_limited(list, n=100, xlabel="", ylabel="", plot_im_name="plot.png"):
+	"Plot (if possible) n first elements from a list and export a png image"
+
+	if n > len(list):
+		n = len(list)
+
+	plt.plot(list[:n])
+	plt.xlabel(xlabel)
+	plt.ylabel(ylabel)
+	plt.ticklabel_format(style="sci", axis="y", scilimits=(0,0))
+
+	plt.savefig(plot_im_name)
+
+
+def plot_cn(noise_sample, n=100):
+	"Plot the graph of a colored noise (n step) and export a png image"
+
+	plot_list_limited(noise_sample, n, "Step", "Volume", "cn_plot.png")
+
+
+def plot_cn_ps(ps_noise_sample, n=100):
+	"Plot the graph of the power spectrum of a colored noise\
+	(first n frequencies) and export a png image"
+
+	plot_list_limited(ps_noise_sample, n, "Frequency", "Volume", "cn_ps_plot.png")
